fdb516e695d819bf32000d3c22e6553845c8cd9a
devLeopar <46007407+devLeopar@users.noreply.github.com>
2020-01-13 17:05:23 +0300

    created time added to quickstart

--- quickstart.php
+++ quickstart.php
@@ -1,10 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-
 /*if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
 }*/
-/*really sorry*/
+/* Above lines is required to make token.json via using cli - CLI kullanarak drive'a giriş yapılıp token.json oluşturulması için öncelikle CLI ile çağrılsın*/
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
@@ -17,7 +16,6 @@ function getClient()
     $client->setAuthConfig('credentials.json');
     $client->setAccessType('offline');
     $client->setPrompt('select_account consent');
-
     // Load previously authorized token from a file, if it exists.
     // The file token.json stores the user's access and refresh tokens, and is
     // created automatically when the authorization flow completes for the first
@@ -27,7 +25,6 @@ function getClient()
         $accessToken = json_decode(file_get_contents($tokenPath), true);
         $client->setAccessToken($accessToken);
     }
-
     // If there is no previous token or it's expired.
     if ($client->isAccessTokenExpired()) {
         // Refresh the token if possible, else fetch a new one.
@@ -39,11 +36,9 @@ function getClient()
             printf("Open the following link in your browser:\n%s\n", $authUrl);
             print 'Enter verification code: ';
             $authCode = trim(fgets(STDIN));
-
             // Exchange authorization code for an access token.
             $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
             $client->setAccessToken($accessToken);
-
             // Check to see if there was an error.
             if (array_key_exists('error', $accessToken)) {
                 throw new Exception(join(', ', $accessToken));
@@ -58,27 +53,29 @@ function getClient()
     return $client;
 }
 
-
-// Get the API client and construct the service object.
-$client = getClient();
+// Get the API client and construct the service object. /////////////
+$client = getClient(); 
 $service = new Google_Service_Drive($client);
+/* Above 2 lines indicates successful data from the object formed with google client api */
+
+/* Those informations should be used in search query to point out spesific folder - Aşağıdaki terimler search query'de folder içini belirtmek için kullanılabilir */
+//$folderName = "scheduling"; // Please set the folder name here.
+//$folderId = "0B49-5XKwF3IJWG8yNHFhWWc4a00"; //scheduling folder Id
 
 
-
-// Print the names and IDs for up to 10 files.
-$folderName = "scheduling"; // Please set the folder name here.
 $optParams = array(
-  'pageSize' => 10,
-  'fields' => 'nextPageToken, files(id, name)',
-  'q' => "mimeType = 'application/vnd.google-apps.spreadsheet'"
+  'pageSize' => 20,
+ // 'fields' => 'nextPageToken, files(*)', // tüm parametrelerini çağır / get all parameters of files,folders specified in search term
+  'fields' => 'nextPageToken, files(id,name,createdTime)',
+  'q' => "mimeType = 'application/vnd.google-apps.spreadsheet'" // only call excel spreadsheets - sadece excel sheetleri döndür
 );
-$results = $service->files->listFiles($optParams);
 
+$results = $service->files->listFiles($optParams);
 if (count($results->getFiles()) == 0) {
     print "No files found.\n";
 } else {
     print "Files:\n";
     foreach ($results->getFiles() as $file) {
-        printf("%s (%s)\n", $file->getName(), $file->getId());
+        printf("%s (%s) %s\n", $file->getName(), $file->getId(),$file->getCreatedTime());
     }
 }
